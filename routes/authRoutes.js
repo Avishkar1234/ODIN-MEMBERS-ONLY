80828c95dac577622acd97f897d4878773366248
Avishkar <avishkar1234567890@gmail.com>
2026-02-23 19:01:24 +0530

    feat: implement membership system with secret passcode and role update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const passport = require("passport");
+const pool = require("../config/db")
+
+const { ensureAuthenticated } = require("../middleware/authMiddleware");
 
 router.get("/signup", authController.signup_get);
 router.post("/signup", authController.signup_post);
@@ -22,6 +25,23 @@ router.get("/logout" , (req, res) => {
         if (err) return next(err);
         res.redirect("/");
     });
+});
+
+router.get("/join", ensureAuthenticated, (req, res) => {
+    res.render("join", { error: null });
+})
+
+router.post("/join", ensureAuthenticated, async (req, res) => {
+    if (req.body.passcode === process.env.MEMBER_PASSCODE) {
+        await pool.query(
+            "UPDATE users SET is_member = true WHERE id = $1",
+            [req.user.id]
+        );
+
+        res.redirect("/");
+    } else {
+        res.render("join", { error: "Incorrect passcode" });
+    }
 });
 
 module.exports = router;
