1ff1eb14a074fc6538068ab43bd2c16d038970e7
Avishkar <avishkar1234567890@gmail.com>
2026-09-20 19:25:45 +0530

    chore: add next to logout authentication route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const passport = require("passport");
-const pool = require("../config/db")
+const pool = require("../config/db");
 
 const { ensureAuthenticated } = require("../middleware/authMiddleware");
 
@@ -10,38 +10,38 @@ router.get("/signup", authController.signup_get);
 router.post("/signup", authController.signup_post);
 
 router.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
-router.post("/login", 
-    passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/login",
-    })
-)
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
+);
 
-router.get("/logout" , (req, res) => {
-    req.logout(function (err) {
-        if (err) return next(err);
-        res.redirect("/");
-    });
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) return next(err);
+    res.redirect("/");
+  });
 });
 
 router.get("/join", ensureAuthenticated, (req, res) => {
-    res.render("join", { error: null });
-})
+  res.render("join", { error: null });
+});
 
 router.post("/join", ensureAuthenticated, async (req, res) => {
-    if (req.body.passcode === process.env.MEMBER_PASSCODE) {
-        await pool.query(
-            "UPDATE users SET is_member = true WHERE id = $1",
-            [req.user.id]
-        );
+  if (req.body.passcode === process.env.MEMBER_PASSCODE) {
+    await pool.query("UPDATE users SET is_member = true WHERE id = $1", [
+      req.user.id,
+    ]);
 
-        res.redirect("/");
-    } else {
-        res.render("join", { error: "Incorrect passcode" });
-    }
+    res.redirect("/");
+  } else {
+    res.render("join", { error: "Incorrect passcode" });
+  }
 });
 
 module.exports = router;
